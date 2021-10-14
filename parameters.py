94c226fd4e542366a98772c9204c46a7eea046f4
Till-Wo <81975116+Till-Wo@users.noreply.github.com>
2021-10-14 08:43:24 +0200

    Richtiges Ordnen der Daten

--- parameters.py
+++
@@ -1,8 +0,0 @@
-"""
-This file contains all environment based parameters used for training
-"""
-ENV_NAME = "CartPole-v1"
-has_continuous_action_space = False
-max_ep_len = 500
-reward_bound = 10000
-time_length= 0.5
